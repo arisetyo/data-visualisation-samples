1f3ebe31ad4abb56d5b7c64d28266805faa3c8ab
Arie M. Prasetyo <ari.setyo@gmail.com>
2022-03-27 15:24:54 +0700

    refactoring the samples navigation system

--- charts/1-linechart.js
+++ charts/1-linechart.js
@@ -1,6 +1,5 @@
 import '../styles/1-linechart.css'
 
 document.querySelector('#chart').innerHTML = `
-  <a href="..">back</a>
-  <h1>Simple line chart <em>(TBA)</em></h1>
+  TBA
 `
--- main.js
+++
@@ -1,7 +0,0 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <h1>Data visualisation samples</h1>
-  <h2>Using D3</h2>
-  <a href="samples/1-linechart.html">1 - Simple line chart</a>
-`
